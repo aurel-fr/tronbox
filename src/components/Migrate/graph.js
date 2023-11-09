628a9cfc6aa01b46c7be7fe2169c087e1d054103
aurel-fr <105201452+aurel-fr@users.noreply.github.com>
2023-11-09 11:39:30 +0800

    feat: build and traverse AST

--- src/components/Migrate/graph.js
+++ src/components/Migrate/graph.js
@@ -1,8 +1,12 @@
 const path = require('path');
 const toposort = require('toposort');
+const parser = require('@babel/parser');
+const traverse = require('@babel/traverse').default;
+const t = require('@babel/types');
+const fs = require('fs');
 
 module.exports = {
-  build: migrations => {
+  build(migrations) {
     const fileNames = new Map();
     for (const migration of migrations) {
       const fullPath = migration.file;
@@ -14,21 +18,40 @@ module.exports = {
     const graph = [];
     for (const migration of migrations) {
       const { file: filePath } = migration;
-      let deployFunc;
-      process.stdout.write('looking for dependency in ' + filePath + '\n');
-      try {
-        delete require.cache[filePath]; // ensure we reload it every time, so changes are taken in consideration
-        deployFunc = require(filePath);
-        for (const dep of deployFunc.dependencies || []) {
-          const depPath = fileNames.get(dep);
-          if (!depPath) throw new Error('could not find the dependency path');
-          graph.push([filePath, depPath]);
-        }
-      } catch (e) {
-        throw new Error('require failed:\n' + e);
+      const deps = this.extractDeps(filePath);
+      for (const dep of deps) {
+        const depPath = fileNames.get(dep);
+        if (!depPath) throw new Error('could not find the dependency path');
+        graph.push([filePath, depPath]);
       }
     }
     return graph;
   },
-  toposort: (nodes, edges) => toposort.array(nodes, edges)
+  toposort: (nodes, edges) => toposort.array(nodes, edges),
+  extractDeps: filePath => {
+    // Read the file content
+    const content = fs.readFileSync(filePath, 'utf8');
+
+    // Parse the content into an AST
+    const ast = parser.parse(content);
+
+    // Traverse the AST to find the dependencies assignment
+    let dependencies = [];
+    traverse(ast, {
+      AssignmentExpression(path) {
+        // Use @babel/types to check for assignments to module.exports.dependencies
+        if (
+          t.isMemberExpression(path.node.left) &&
+          t.isMemberExpression(path.node.left.object) &&
+          t.isIdentifier(path.node.left.object.object, { name: 'module' }) &&
+          t.isIdentifier(path.node.left.object.property, { name: 'exports' }) &&
+          t.isIdentifier(path.node.left.property, { name: 'dependencies' }) &&
+          t.isArrayExpression(path.node.right)
+        ) {
+          dependencies = path.node.right.elements.map(element => element.value);
+        }
+      }
+    });
+    return dependencies;
+  }
 };
